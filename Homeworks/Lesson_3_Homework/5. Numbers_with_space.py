7c2ba56cd632a89f6ae4b93ec60ed7a111bc0e14
Morfi <marfutin.00@mail.ru>
2022-04-12 06:24:52 +0300

    Убрал лишние пробелы в пятой задаче

--- Homeworks/Lesson_3_Homework/5. Numbers_with_space.py
+++ Homeworks/Lesson_3_Homework/5. Numbers_with_space.py
@@ -1,10 +1,8 @@
 num_sum = 0
 while True:
-
     my_numbers = input('Введите числа через пробел (или введите q для выхода): \n').split()
     my_simbols = ''.join(my_numbers)
     if my_simbols.isdigit():
-
         num_sum += sum(map(int,my_numbers))
         print(num_sum)
     elif my_simbols == 'q':
