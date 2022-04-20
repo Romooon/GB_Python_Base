6f09de6ddbef9e596741ebb5bead88c859804b05
Morfi <marfutin.00@mail.ru>
2022-04-20 21:03:50 +0300

    Поправил домашку по пятому уроку

--- Homeworks/Lesson_5_Homework/1. Programm_file.py
+++ Homeworks/Lesson_5_Homework/1. Programm_file.py
@@ -2,8 +2,24 @@
 # вводимые пользователем. Об окончании ввода данных будет свидетельствовать пустая
 # строка.
 
-with open('program_file.txt', 'w', encoding='utf-8') as pf:
-    my_str = None
-    while not my_str == '':
-        my_str = input('Поочерёдно вводите строки для записи: ')
-        pf.writelines(my_str + '\n')
+# with open('program_file.txt', 'w', encoding='utf-8') as pf:
+#     my_str = None
+#     while not my_str == '':
+#         my_str = input('Поочерёдно вводите строки для записи (Для выхода нажмите Enter): ')
+#         pf.writelines(my_str + '\n')
+#
+# После проверки
+
+# with open('program_file.txt', 'w', encoding='utf-8') as f:
+#     while True:
+#         line = input('Введите текст, для выхода Enter: ')
+#         if not line:
+#             break
+#         print(line, file=f)
+
+# Ещё вариант
+#
+print('Введите данны для записи в файл. \nДля окончания ввода введите пустую строку')
+with open('program_file.txt', 'w', encoding='utf-8') as my_f:
+    while (line := input("Введите строку: ")) != '':
+        my_f.write(f"{line}\n")
--- Homeworks/Lesson_5_Homework/2. Count_my_strings.py
+++ Homeworks/Lesson_5_Homework/2. Count_my_strings.py
@@ -2,6 +2,9 @@
 # подсчёт строк и слов в каждой строке.
 
 with open('Warrior.txt', 'r', encoding='utf-8') as f:
+    lines = 0
     for line in f:
         print(line, f'Слов в строке: {len(line.split())}', end='')
         print()
+        lines += 1
+print(f'Строк в файле: {lines}', end='')
